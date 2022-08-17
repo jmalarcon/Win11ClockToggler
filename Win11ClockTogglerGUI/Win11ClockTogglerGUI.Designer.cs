695125f1dad7e19119ea2344b3c144c6c6adc1be
José M. Alarcón <jmalarcon@jasoft.org>
2022-08-17 12:28:00 +0200

    Fixed some things
    - Fixed settings for DpiAwareness
    - Restored app icon

--- Win11ClockTogglerGUI/Win11ClockTogglerGUI.Designer.cs
+++ Win11ClockTogglerGUI/Win11ClockTogglerGUI.Designer.cs
@@ -261,8 +261,9 @@
             // 
             this.AcceptButton = this.btnHideShow;
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
             this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(683, 193);
             this.Controls.Add(this.cmdAbout);
