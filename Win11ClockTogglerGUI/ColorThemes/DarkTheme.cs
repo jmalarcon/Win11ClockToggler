a5abfefc334431b7c0cd4b06eb1835fbf6feb779
José M. Alarcón <jmalarcon@jasoft.org>
2022-12-31 17:59:14 +0100

    Changed Dark & Light theme colors to fit Visual Studio's (as a reference)

--- Win11ClockTogglerGUI/ColorThemes/DarkTheme.cs
+++ Win11ClockTogglerGUI/ColorThemes/DarkTheme.cs
@@ -7,13 +7,14 @@ using System.Threading.Tasks;
 
 namespace Win11ClockTogglerGUI.ColorThemes
 {
-
+    // Color references:
+    // https://learn.microsoft.com/en-us/visualstudio/extensibility/ux-guidelines/color-value-reference-for-visual-studio?view=vs-2022
     public class DarkTheme : Theme
     {
-        public override Color Foreground { get => Color.FromArgb(227, 227, 227); }
-        public override Color PanelBackground { get => Color.FromArgb(43, 43, 43); }
-        public override Color DisabledForeground { get => Color.FromArgb(120, 120, 120); }
-        public override Color DisabledPanelBackground { get => Color.FromArgb(38, 38, 38); }
-        public override Color Background { get => Color.FromArgb(32, 32, 32); }
+        public override Color Foreground { get => Color.FromArgb(255, 241, 241, 241); }
+        public override Color PanelBackground { get => Color.FromArgb(255, 37, 37, 38); }
+        public override Color DisabledForeground { get => Color.FromArgb(255, 101, 101, 101); }
+        public override Color DisabledPanelBackground { get => Color.FromArgb(255, 37, 38, 38); }
+        public override Color Background { get => Color.FromArgb(255, 45, 45, 48); }
     }
 }
--- Win11ClockTogglerGUI/ColorThemes/LightTheme.cs
+++ Win11ClockTogglerGUI/ColorThemes/LightTheme.cs
@@ -3,17 +3,19 @@ using System.Windows.Forms;
 
 namespace Win11ClockTogglerGUI.ColorThemes
 {
+    // Color references:
+    // https://learn.microsoft.com/en-us/visualstudio/extensibility/ux-guidelines/color-value-reference-for-visual-studio?view=vs-2022
     public class LightTheme : Theme
     {
         //Enabled panel Foreground text color
-        public override Color Foreground { get => SystemColors.WindowText; }
+        public override Color Foreground { get => Color.FromArgb(255, 30, 30, 30); }
         //Enabled panel panel background color
-        public override Color PanelBackground { get => SystemColors.ControlLight; }
+        public override Color PanelBackground { get => Color.FromArgb(255, 245, 245, 245); }
         //Disabled panel foreground text color
-        public override Color DisabledForeground { get => SystemColors.InactiveCaptionText; }
+        public override Color DisabledForeground { get => Color.FromArgb(255, 162, 164, 165); }
         //Disabled panel background text color
-        public override Color DisabledPanelBackground { get => SystemColors.InactiveCaption; }
+        public override Color DisabledPanelBackground { get => Color.FromArgb(255, 245, 245, 245); }
         //Form background
-        public override Color Background { get => SystemColors.ControlLightLight; }
+        public override Color Background { get => Color.FromArgb(255, 238, 238, 242); }
     }
 }
